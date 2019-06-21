219d9053ae2fef600f8856843a35562882f86181
Evgeny Ezhov <ezhov.evgeny@gmail.com>
2019-06-21 15:20:25 +0300

    Add webdav container

--- tests/webdav/test_connection.py
+++ tests/webdav/test_connection.py
@@ -6,6 +6,6 @@ import allure
 class TestRequiredOptions:
 
     def test_without_webdav_hostname(self):
-        options = { 'webdav_server': "https://webdav.yandex.ru"}
+        options = { 'webdav_server': "http://localhost:8888/webdav"}
         allure.attach('options', options.__str__())
         assert 1
